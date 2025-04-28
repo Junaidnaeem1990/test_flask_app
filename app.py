4ac3d4992fe6f5ae53e3301d77832c828b6b27b5
Junaid Naeem <junaidnaeem1990@gmail.com>
2025-04-27 18:18:22 -0700

    updating last commits

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask,render_template,request 
 
 app =Flask(__name__)
 
-@app.route("/") #homepage
+@app.route("/",methods=['GET','POST']) #homepage
 def home():
-    return "Hello Python with Flask" 
+    # name = input("Enter You Name")
+    # if request.method =="POST":
+    #     names = request.form.get('name')
+    # return render_template("Home.html", names = name)  #only one position argument but key word  can be many # data must be in dictionary form
+    name = input("Enter You Name")
+    return render_template("Home.html", data = {"name":name}) 
+
+@app.route("/about",methods=['GET','POST']) #aboutpage
+def about():
+    if request.method=="POST":
+        num1=int(request.form['num1'])
+        num2=int(request.form['num2'])
+        results  = num1+num2
+                #reciving data
+    return render_template("About.html",result=results)
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
